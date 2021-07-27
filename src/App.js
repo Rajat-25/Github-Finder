788dee270b547e89694d5e218339579106b7d476
Rajat <rajatkr125@gmail.com>
2021-07-27 10:44:19 +0530

    Get Repo | Class component ends

--- src/App.js
+++ src/App.js
@@ -12,18 +12,11 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos:[],
     loading: false,
     alert: null,
-    user: {},
   };
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-  //   this.setState({ users: res.data, loading: false });
-  // }
 
   // Search github users
 
@@ -33,6 +26,15 @@ class App extends Component {
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ users: res.data.items, loading: false });
+  };
+
+  //Get User repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
   };
 
   //Get single user
@@ -59,7 +61,7 @@ class App extends Component {
   };
 
   render() {
-    const { users,user,loading} = this.state;
+    const { users,user,repos,loading} = this.state;
     return (
       <Router>
         <div className='App'>
@@ -87,7 +89,8 @@ class App extends Component {
                 exact
                 path='/user/:login'
                 render={(props) => (
-                  <User {...props} getUser={this.getUser} user={user} />
+                  <User {...props} getUser={this.getUser} 
+                  getUserRepos={this.getUserRepos} repos={repos} user={user} loading={loading} />
                 )}
               />
             </Switch>
